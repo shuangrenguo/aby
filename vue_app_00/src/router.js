9ce6cc70bbf451cf98f4260d884d4358159f2a70
TianYongShun <TianYongShun69@163.com>
2019-08-13 20:49:33 +0800

    心愿单

--- vue_app_00/src/router.js
+++ vue_app_00/src/router.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloContainer from "./components/HelloWorld.vue"
+import Wish from "./components/tian/Wish.vue"
 
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {path:'/Wish',component:Wish},
     {path:'/',component:HelloContainer},
   ]
 })
