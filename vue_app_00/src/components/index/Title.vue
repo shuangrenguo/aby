e713d6185e3acf7917c34b227b44d8e270ed04d5
cjr <1176868991@qq.com>
2019-08-15 19:21:27 +0800

    title

---
+++ vue_app_00/src/components/index/Title.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div class="title">
+            <h5>房源</h5>
+        </div>
+        <div class="inputPlace">
+            <input type="text" placeholder="输入区域、景点">
+        </div>
+        <div class="buttonTime">
+            <button>入住退房日期</button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    .title{
+        margin:10px 20px 20px 15px;
+    }
+
+    .inputPlace{
+        width:325px;
+        height:40px;
+        margin:auto;
+        margin-bottom:8px;
+    }
+    .inputPlace input{
+        width:325px;
+        height:40px;
+        font-size: 18px;
+        padding-left:40px;
+        
+    }
+    .buttonTime{
+        width:325px;
+        height:40px;
+        margin:auto;
+    }
+    .buttonTime button{
+        width:325px;
+        height:40px;
+    }
+</style>
