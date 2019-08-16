aaaca62dda681cc1f57e25b4ea11864f7de5a548
cjr <1176868991@qq.com>
2019-08-16 20:51:30 +0800

    主页

---
+++ vue_app_00/src/components/index/indexHouse.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="houses">
+        <!-- 第一个房源 -->
+        <div class="house">
+            <div class="imgpro">
+                <img src="../../assets/house1.jpg">
+            </div>
+            <div>
+                <span>整套公寓·杭州</span>
+            </div>
+            <div>
+                <h6>【恃风舍】温馨风 市中心 长寿路、江宁路地铁口</h6>
+            </div>
+            <div>
+                <span>￥367 <s>￥408</s>/晚</span>
+            </div>
+            <div>
+                <p><span>☆☆☆☆☆</span>8、超赞房东</p>
+            </div>
+        </div>
+        <!-- 第二个房源 -->
+        <div class="house">
+            <div class="imgpro">
+                <img src="../../assets/house1.jpg">
+            </div>
+            <div>
+                <span>整套公寓·杭州</span>
+            </div>
+            <div>
+                <h6>【恃风舍】温馨风 市中心 长寿路、江宁路地铁口</h6>
+            </div>
+            <div>
+                <span>￥367 <s>￥408</s>/晚</span>
+            </div>
+            <div>
+                <p><span>☆☆☆☆☆</span>8、超赞房东</p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.houses{
+    width:100%;
+    display: flex;
+    justify-content:space-around;
+}
+.house{
+    width: 45%;
+    
+}
+.imgpro{
+    width:100%;
+}
+.imgpro img{
+    width:100%;
+}
+</style>
--- vue_app_00/src/router.js
+++ vue_app_00/src/router.js
@@ -2,9 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloContainer from "./components/HelloWorld.vue"
 //引入组件
-import Carousel from "./components/index/Carousel.vue"
 import index from "./components/index/index.vue"
-import title from "./components/index/Title.vue"
 
 import { Swipe, SwipeItem } from 'mint-ui';
 Vue.component(Swipe.name, Swipe);
@@ -15,8 +13,6 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {path:'/',component:HelloContainer},
-    {path:'/carousel',component:Carousel},
     {path:'/index',component:index},
-    {path:'/title',component:title},
   ]
 })
